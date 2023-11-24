ca4bad0c44f83af795dd231ba6b236e2ba87bde3
valentinptr <valentin.ptr@icloud.com>
2023-11-24 16:37:27 +0100

    Interaction improvment

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from tensorflow import keras
-import pandas as pd
+from enum import Enum
+
 import numpy as np
+import pandas as pd
 from fastapi import FastAPI
-from pydantic import BaseModel
+from tensorflow import keras
 
 
 def rule(player, ia):
@@ -22,7 +23,7 @@ def rule(player, ia):
         return "Player Won"
 
 
-def translate(choice):
+def int_to_str(choice):
     if choice == 0:
         return 'Rock'
     elif choice == 1:
@@ -31,8 +32,19 @@ def translate(choice):
         return 'Scissor'
 
 
-class Decision(BaseModel):
-    player_choice: int
+def str_to_int(choice):
+    if choice is Decision.rock:
+        return 0
+    elif choice is Decision.paper:
+        return 1
+    else:
+        return 2
+
+
+class Decision(str, Enum):
+    rock = "Rock"
+    paper = "Paper"
+    scissor = "Scissor"
 
 
 app = FastAPI()
@@ -40,13 +52,14 @@ app = FastAPI()
 
 @app.post("/predict")
 async def predict_game(decision: Decision):
+    player_choice = str_to_int(decision)
     saved_model = keras.models.load_model('model/model2.h5')
     df_save = pd.read_csv('model/df_save.csv')
 
     mean_x = np.array([df_save['mean_x0'][0], df_save['mean_x1'][0]])
     std_x = np.array([df_save['std_x0'][0], df_save['std_x1'][0]])
 
-    my_data = ([decision.player_choice, 1] - mean_x) / std_x
+    my_data = ([player_choice, 1] - mean_x) / std_x
 
     my_data = np.array(my_data).reshape(1, 2)
 
@@ -59,6 +72,6 @@ async def predict_game(decision: Decision):
     else:
         choixIA = 2
 
-    output = rule(decision.player_choice, choixIA)
+    output = rule(player_choice, choixIA)
 
-    return {"Prediction : ", translate(choixIA), "Result : ", output}
+    return {"Prediction : ", int_to_str(choixIA), "Result : ", output}
